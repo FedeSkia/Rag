2688b8446269538831f657478b52a0c3cb6cd424
Federico Conoci <federico.conoci@musement.com>
2025-08-05 20:16:57 +0200

    streaming out msgs

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+import asyncio
 from random import random
+
 from langchain_core.runnables import RunnableConfig
 
 from graph import create_graph
+
+
+def launch_graph(graph, input_message):
+    for message_chunk, metadata in graph.stream(
+            {"messages": input_message},
+            stream_mode="messages",
+    ):
+        if message_chunk.content:
+            print(message_chunk.content, "|", flush=True)
+
 
 if __name__ == '__main__':
     config: RunnableConfig = {"configurable": {"thread_id": random()}}
     graph = create_graph()
     input_message = "What is Task Decomposition?"
-
-    for step in graph.stream(
-            {"messages": [{"role": "user", "content": input_message}]},
-            stream_mode="values",
-    ):
-        print(step)
-
+    launch_graph(graph, input_message)
