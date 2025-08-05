0aeb6a3ea1ccccd39e439820252c86a0893e4ecd
Federico Conoci <federico.conoci@musement.com>
2025-08-05 16:12:59 +0200

    refactoring

---
+++ db.py
@@ -0,0 +1,33 @@
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_postgres import PGVector
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
+import bs4
+
+
+def index_document(vector_store: PGVector):
+    # Load and chunk contents of the blog
+    loader = WebBaseLoader(
+        web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+        bs_kwargs=dict(
+            parse_only=bs4.SoupStrainer(
+                class_=("post-content", "post-title", "post-header")
+            )
+        ),
+    )
+    docs = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    all_splits = text_splitter.split_documents(docs)
+    # Index chunks
+    _ = vector_store.add_documents(documents=all_splits)
+
+
+def create_vector_store() -> PGVector:
+    embeddings = OllamaEmbeddings(
+        model="qwen3:0.6b",
+    )
+    return PGVector(
+        embeddings=embeddings,
+        collection_name="my_docs",
+        connection="postgresql://langgraph:langgraph@localhost:5433/postgres?sslmode=disable",
+    )
---
+++ graph.py
@@ -0,0 +1,93 @@
+from typing import TypedDict, List, Annotated
+
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import SystemMessage, BaseMessage
+from langchain_core.tools import tool
+from langgraph.graph import END, StateGraph, add_messages
+from langgraph.graph.state import CompiledStateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
+
+from db import create_vector_store
+
+
+# Define state for application
+class State(TypedDict):
+    messages: Annotated[List[BaseMessage], add_messages]
+
+
+@tool(response_format="content_and_artifact")
+def retrieve(query: str):
+    """Retrieve information related to a query."""
+    vector_store = create_vector_store()
+    retrieved_docs = vector_store.similarity_search(query, k=2)
+    serialized = "\n\n".join(
+        (f"Source: {doc.metadata}\nContent: {doc.page_content}")
+        for doc in retrieved_docs
+    )
+    return serialized, retrieved_docs
+
+
+# Step 1: Generate an AIMessage that may include a tool-call to be sent.
+def query_or_respond(state: State):
+    """Generate tool call for retrieval or respond."""
+    chat_model = init_chat_model(model="ollama:qwen3:0.6b", reasoning=True, num_predict=1000, temperature=0.0)
+    llm_with_tools = chat_model.bind_tools([retrieve])
+    response = llm_with_tools.invoke(state["messages"])
+    # MessagesState appends messages to state instead of overwriting
+    return {"messages": [response]}
+
+
+# Step 3: Generate a response using the retrieved content.
+def generate(state: State):
+    """Generate answer."""
+    # Get generated ToolMessages
+    recent_tool_messages = []
+    for message in reversed(state["messages"]):
+        if message.type == "tool":
+            recent_tool_messages.append(message)
+        else:
+            break
+    tool_messages = recent_tool_messages[::-1]
+
+    # Format into prompt
+    docs_content = "\n\n".join(doc.content for doc in tool_messages)
+    system_message_content = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        f"{docs_content}"
+    )
+    conversation_messages = [
+        message
+        for message in state["messages"]
+        if message.type in ("human", "system")
+           or (message.type == "ai" and not message.tool_calls)
+    ]
+    prompt = [SystemMessage(system_message_content)] + conversation_messages
+
+    # Run
+    chat_model = init_chat_model(model="ollama:qwen3:0.6b", reasoning=True, num_predict=1000, temperature=0.0)
+    response = chat_model.invoke(prompt)
+    return {"messages": [response]}
+
+
+def create_graph() -> CompiledStateGraph:
+    graph_builder = StateGraph(State)
+    graph_builder.add_node(query_or_respond)
+    tools = ToolNode([retrieve])
+    graph_builder.add_node(tools)
+    graph_builder.add_node(generate)
+
+    graph_builder.set_entry_point("query_or_respond")
+    graph_builder.add_conditional_edges(
+        "query_or_respond",
+        tools_condition,
+        {END: END, "tools": "tools"},
+    )
+    graph_builder.add_edge("tools", "generate")
+    graph_builder.add_edge("generate", END)
+
+    return graph_builder.compile()
