a14160319b04e5330d3c546f2190509c3c266c58
Federico Conoci <federico.conoci@musement.com>
2025-08-06 19:45:08 +0200

    added web server with streaming response of token

---
+++ controller.py
@@ -0,0 +1,52 @@
+from random import random
+
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from langchain_core.runnables import RunnableConfig
+from pydantic import BaseModel
+
+from graph import create_graph
+from main import launch_graph
+
+app = FastAPI()
+
+class InputData(BaseModel):
+    content: str
+
+
+if __name__ == '__main__':
+    # initilizing our application
+    app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:3000"],  # Your frontend URL
+        allow_credentials=True,
+        allow_methods=["*"],  # Allows all methods
+        allow_headers=["*"],  # Allows all headers
+    )
+
+    config: RunnableConfig = {"configurable": {"thread_id": random()}}
+    graph = create_graph()
+
+    # curl example: curl --location 'localhost:8000/invoke' \
+    # --header 'Content-Type: application/json' \
+    # --data '{
+    #     "content": "hello"
+    # }'
+    @app.post("/invoke")
+    async def invoke(data: InputData):
+
+        return StreamingResponse(
+            launch_graph(graph, data.content),
+            media_type="text/event-stream",
+            headers={
+                "Cache-Control": "no-cache",
+                "Connection": "keep-alive",
+            }
+        )
+
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-import asyncio
-from random import random
-
-from langchain_core.runnables import RunnableConfig
-
-from graph import create_graph
 
 
-def launch_graph(graph, input_message):
+async def launch_graph(graph, input_message) -> str:
     for message_chunk, metadata in graph.stream(
             {"messages": input_message},
             stream_mode="messages",
     ):
         if message_chunk.content:
-            print(message_chunk.content, "|", flush=True)
-
-
-if __name__ == '__main__':
-    config: RunnableConfig = {"configurable": {"thread_id": random()}}
-    graph = create_graph()
-    input_message = "What is Task Decomposition?"
-    launch_graph(graph, input_message)
+            yield message_chunk.content
