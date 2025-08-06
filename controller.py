eb55a9d183709c5e5f8c455b99877c22e832bc3f
Federico Conoci <federico.conoci@musement.com>
2025-08-06 19:50:15 +0200

    refactored

--- controller.py
+++ controller.py
@@ -1,9 +1,9 @@
 from random import random
 
-from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
-from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
 from langchain_core.runnables import RunnableConfig
 from pydantic import BaseModel
 
@@ -11,41 +11,37 @@ from graph import create_graph
 from main import launch_graph
 
 app = FastAPI()
+# initilizing our application
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+config: RunnableConfig = {"configurable": {"thread_id": random()}}
+graph = create_graph()
 
 
 class InputData(BaseModel):
     content: str
 
 
-if __name__ == '__main__':
-    # initilizing our application
-    app = FastAPI()
-
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["http://localhost:3000"],  # Your frontend URL
-        allow_credentials=True,
-        allow_methods=["*"],  # Allows all methods
-        allow_headers=["*"],  # Allows all headers
+# curl -N -X POST http://localhost:8000/invoke \
+#   -H "Content-Type: application/json" \
+#   -d '{"content": "hello"}'
+@app.post("/invoke")
+async def invoke(data: InputData):
+    return StreamingResponse(
+        launch_graph(graph, data.content),
+        media_type="text/event-stream",
+        headers={
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive",
+        }
     )
 
-    config: RunnableConfig = {"configurable": {"thread_id": random()}}
-    graph = create_graph()
 
-
-    # curl -N -X POST http://localhost:8000/invoke \
-    #   -H "Content-Type: application/json" \
-    #   -d '{"content": "hello"}'
-    @app.post("/invoke")
-    async def invoke(data: InputData):
-        return StreamingResponse(
-            launch_graph(graph, data.content),
-            media_type="text/event-stream",
-            headers={
-                "Cache-Control": "no-cache",
-                "Connection": "keep-alive",
-            }
-        )
-
-
+if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
